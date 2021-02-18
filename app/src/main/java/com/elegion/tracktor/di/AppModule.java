891ded487ced90dce01ceae0fe2f701e8e2b2838
savelgmx <savel@gmx.com>
2021-02-18 16:28:17 +0700

    Context in AppModule according to: https://medium.com/swlh/toothpick-3-a-hidden-gem-194ae6ee8671 //TODO rewrite Context Inject(perharps it will be not nessesary)
    ReadPreferences MContext null not App Context
    //TODO make it not null
    //TODO сделать инжект context во вьюмодель через туспик передавать

--- app/src/main/java/com/elegion/tracktor/di/AppModule.java
+++ app/src/main/java/com/elegion/tracktor/di/AppModule.java
@@ -1,5 +1,7 @@
 package com.elegion.tracktor.di;
 
+import android.content.Context;
+
 import com.elegion.tracktor.App;
 import com.elegion.tracktor.ui.map.MainViewModel;
 
@@ -10,9 +12,14 @@ public class AppModule extends Module {
 
     public AppModule(App app) {
         this.mApp = app;
+
+// https://medium.com/swlh/toothpick-3-a-hidden-gem-194ae6ee8671
+        Context context = app.getApplicationContext();
+
         bind(App.class).toInstance(mApp);
         // нужно в AppModule определенно прописать
         bind(MainViewModel.class);
+        bind(Context.class).toInstance(context);
     }
 
     App provideApp() {
