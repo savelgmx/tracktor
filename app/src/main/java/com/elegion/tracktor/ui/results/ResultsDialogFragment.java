3e1cac84acda5ce781003c9fc447d7025fc4840b
savelgmx <savel@gmx.com>
2021-03-26 14:04:26 +0700

    Убрал ненужные импорты из ResultsDialogFragment  Removed noFactory Found Exeption  at com.elegion.tracktor.ui.results.ResultsDialogFragment.onCreateDialog(ResultsDialogFragment.java:80)
    //TODO  Введенный комментарий должен отображаться на экране.Если комментария нет, то отображать текст “Введите комментарий”. При повторном нажатии на кнопку изменения комментария в диалоге должен отображаться текущий комментарий.

--- app/src/main/java/com/elegion/tracktor/ui/results/ResultsDialogFragment.java
+++ app/src/main/java/com/elegion/tracktor/ui/results/ResultsDialogFragment.java
@@ -3,7 +3,6 @@ package com.elegion.tracktor.ui.results;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
-import android.arch.lifecycle.ViewModelStoreOwner;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,14 +12,11 @@ import android.widget.EditText;
 
 import com.elegion.tracktor.App;
 import com.elegion.tracktor.R;
-import com.elegion.tracktor.di.DialogFragmentModule;
-import com.elegion.tracktor.di.DialogFragmentViewModelModule;
-import com.elegion.tracktor.di.ViewModelModule;
 import javax.inject.Inject;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import toothpick.Scope;
+
 import toothpick.Toothpick;
 
 //отображает диалоговое окно для ввода и редактироания комментария к пройденному треку
@@ -35,15 +31,6 @@ public class ResultsDialogFragment extends DialogFragment {
     @Inject
     DialogFragmentViewModel mDialogFragmentViewModel;
 
-    /*
-    @Inject
-    ResultsViewModel mResultsViewModel;//ResultsViewModel должен инжектиться в ResultsFragment
-*/
-
-/*
-    @Inject
-    RealmRepository mRealmRepository;
-*/
 
 
 
@@ -53,7 +40,7 @@ public class ResultsDialogFragment extends DialogFragment {
         Log.d("ResultsDetailFragment","OnClickListener with Comment= "+ ibAddCommentText.getText().toString());
         Log.d("ResultsDetailFragment","mDialogFragmentViewModel="+String.valueOf(mDialogFragmentViewModel));
 
-     //    mDialogFragmentViewModel.updateComment(mTrackId,ibAddCommentText.getText().toString());
+         mDialogFragmentViewModel.updateComment(mTrackId,ibAddCommentText.getText().toString());
 
 
 
@@ -73,13 +60,6 @@ public class ResultsDialogFragment extends DialogFragment {
 
     public Dialog onCreateDialog(Bundle savedInstanceState) {
 
-        //TODO remove noFactory Found Exeption
-        // at com.elegion.tracktor.ui.results.ResultsDialogFragment.onCreateDialog(ResultsDialogFragment.java:78)
-/*
-        Scope scope = Toothpick.openScope(ResultsDialogFragment.class);
-        scope.installModules(new DialogFragmentViewModelModule());
-        Toothpick.inject(this, scope);
-*/
         Toothpick.inject(this,App.getAppScope());
 
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
