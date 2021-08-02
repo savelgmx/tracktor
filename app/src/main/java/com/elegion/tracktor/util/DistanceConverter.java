8b665c9131fc0f9e5891b55fe9b2bdd45de87557
savelgmx <savel@gmx.com>
2021-08-02 15:17:18 +0700

    new branch for new task в настройки единиц измерения расстояния пункты м/км и футы/мили; вспомогательный (util) класс DistanceConverter в Настройки выбор степени сжатия скриншота карты //TODO конвертацию расстояния на все экраны, в которых присутствует величина пройденного расстояния в единицы измерения, выбранные на экране Настройки; //TODO toBase64() класса ScreenshotMaker compression_ratio

---
+++ app/src/main/java/com/elegion/tracktor/util/DistanceConverter.java
@@ -0,0 +1,13 @@
+package com.elegion.tracktor.util;
+/*
+вспомогательный (util) класс DistanceConverter,
+который будет осуществлять конвертацию из метров в футы/мили.
+ */
+
+public class DistanceConverter {
+
+    public int convertDistanceToFootsMiles(int distance){
+        return distance;
+    }
+
+}
