1dd78830971bf129d2ea71efeef54e40c9f7e758
savelgmx <savel@gmx.com>
2021-09-20 12:45:29 +0700

    Новая ветвь Добавлены классы NotificationHelper и TrackHelper //TODO наполнить классы NotificationHelper и TrackHelper

---
+++ app/src/main/java/com/elegion/tracktor/service/NotificationHelper.java
@@ -0,0 +1,8 @@
+package com.elegion.tracktor.service;
+/*
+Всю логику, связанную с созданием уведомления
+переносим из CounterService
+в отдельный класс - NotificationHelper
+ */
+public class NotificationHelper {
+}
---
+++ app/src/main/java/com/elegion/tracktor/service/TrackHelper.java
@@ -0,0 +1,7 @@
+package com.elegion.tracktor.service;
+/*
+Всю логику, связанную с расчетом и обновлением маршрута
+переносим из CounterService в отдельный класс - TrackHelper
+ */
+public class TrackHelper {
+}
