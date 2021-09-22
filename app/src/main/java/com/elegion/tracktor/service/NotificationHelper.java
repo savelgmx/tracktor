f70fe6167ce10111e379b8b24caf73853064164e
savelgmx <savel@gmx.com>
2021-09-22 16:04:45 +0700

    в NotificationHelper ссылки по написанию //TODO наполнить классы NotificationHelper и TrackHelper

--- app/src/main/java/com/elegion/tracktor/service/NotificationHelper.java
+++ app/src/main/java/com/elegion/tracktor/service/NotificationHelper.java
@@ -4,5 +4,10 @@ package com.elegion.tracktor.service;
 переносим из CounterService
 в отдельный класс - NotificationHelper
  */
+
 public class NotificationHelper {
+
+    //https://www.google.com/search?q=notification+helper+android+%D0%BF%D1%80%D0%B8%D0%BC%D0%B5%D1%80&rlz=1C1GCEU_ruRU836RU837&sxsrf=AOaemvJjTqdcasvKMGq3seoWaE9ppPQclg%3A1632111730671&ei=cgxIYeGuKObKrgT0j6TADQ&oq=notification+helper+android+ghbvth&gs_lcp=Cgdnd3Mtd2l6EAEYADIHCCEQChCgAToHCAAQRxCwAzoGCAAQFhAeOgUIIRCgAUoECEEYAFCoMFi8YmDnamgBcAJ4AIABuAGIAbkJkgEDMC43mAEAoAEByAEIwAEB&sclient=gws-wiz
+    //https://habr.com/ru/post/244423/
+
 }
