8bb7a3ba7a83ab0435b48f9760d25b9644a78a2b
savelgmx <savel@gmx.com>
2022-02-09 16:12:43 +0700

    Task 10 tests  unit-тест DistanceConverterTest;  //TODO Оцените степень покрытия тестами класса   DistanceConverter.  тестирование исключений для класса DistanceConverter.

---
+++ app/src/test/java/com/elegion/tracktor/util/DistanceConverterTest.java
@@ -0,0 +1,35 @@
+package com.elegion.tracktor.util;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.Assert.*;
+@RunWith(MockitoJUnitRunner.class)
+
+public class DistanceConverterTest {
+
+    public DistanceConverter mDistanceConverter;
+
+
+
+    @Before
+    public void setUp() throws Exception {
+
+        mDistanceConverter = new DistanceConverter();
+    }
+
+    @Test
+    public void convertFromMetersToKm() {
+
+     }
+
+    @Test
+    public void convertFromMetersToMiles() {
+    }
+
+    @Test
+    public void convertFromMetersToFeets() {
+    }
+}
